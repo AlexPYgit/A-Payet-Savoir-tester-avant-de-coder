f499a69bd0d6502d19f6c3509fb284178f10bfe7
Alexis PAYET <alexis.payet0@gmail.com>
2023-06-12 14:28:30 +0200

    change jar proj en war proj maven

--- sql/DB clients.sql
+++ sql/DB clients.sql
@@ -35,6 +35,27 @@ CREATE TABLE IF NOT EXISTS `TDD`.`clients` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `TDD`.`sexe`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `TDD`.`sexe` ;
+
+CREATE TABLE IF NOT EXISTS `TDD`.`sexe` (
+  `male` VARCHAR(45) NOT NULL,
+  `female` VARCHAR(45) NOT NULL,
+  `primary_key` VARCHAR(45) NOT NULL,
+  `clients_idclient` INT NOT NULL,
+  PRIMARY KEY (`primary_key`, `clients_idclient`),
+  UNIQUE INDEX `primary_key_UNIQUE` (`primary_key` ASC) VISIBLE,
+  INDEX `fk_sexe_clients_idx` (`clients_idclient` ASC) VISIBLE,
+  CONSTRAINT `fk_sexe_clients`
+    FOREIGN KEY (`clients_idclient`)
+    REFERENCES `TDD`.`clients` (`idclient`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
